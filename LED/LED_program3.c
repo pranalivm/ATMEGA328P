3f9863a2352e789fe956d5f38cb41e258c8ea295
Pranali <98734090+pranalivm@users.noreply.github.com>
2024-08-29 17:05:47 +0530

    Update LED_program3.c

--- LED/LED_program3.c
+++ LED/LED_program3.c
@@ -1,24 +1,28 @@
-#define PranaliIOPort (volatile unsigned char *)0x24
-#define PranaliLEDPort (volatile unsigned char *)0x25
+// define macros for memory-mapped registers
+#define PranaliIOPort (volatile unsigned char *)0x24  // points to DDRB (Data Direction Register for PORTB)
+#define PranaliLEDPort (volatile unsigned char *)0x25 // points to PORTB (controls pin states)
+
 
 int main(void)
 {
-	*PranaliIOPort = 0x20;
-	
-	
-	while(1)
-	{
-		unsigned long int i = 0, j = 0;
-	
-		for(i; i < 100000; i++)
-		{
-			*PranaliLEDPort = 0x20;
-		}
-		
-		for(j; j < 100000; j++)
-		{
-			*PranaliLEDPort = 0x00;
-		}
-	}
+    // set pin 13 (PB5) as output by setting the 5th bit of DDRB to 1 (0x20)
+    *PranaliIOPort = 0x20;
+
+    // infinite loop
+    while(1)
+    {
+        unsigned long int i = 0, j = 0;  // declare variables for loop counters
+
+        // first loop to turn the LED ON
+        for(i; i < 100000; i++)
+        {
+            *PranaliLEDPort = 0x20;  // set pin 13 (PB5) HIGH (turn ON LED)
+        }
+
+        // second loop to turn the LED OFF
+        for(j; j < 100000; j++)
+        {
+            *PranaliLEDPort = 0x00;  // set pin 13 (PB5) LOW (turn OFF LED)
+        }
+    }
 }
-	
